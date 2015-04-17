64d4f94be7d794ab9d8a3489511023968ff9981e
Alexey <anazarov94@gmail.com>
2015-04-18 02:40:53 +0600

    ui to py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from PyQt4 import uic, QtGui, QtCore
-
+import mainform as mf
+from PyQt4 import QtCore, QtGui
 import sys
 
+class MyWindow(QtGui.QWidget):
+    def __init__(self,parent=None):
+        QtGui.QWidget.__init__(self,parent)
+        self.ui = mf.Ui_mainform()
+        self.ui.setupUi(self)
+        self.connect(self.ui.pushButton, QtCore.SIGNAL("clicked()"), QtGui.qApp.quit)
 
-app = QtGui.QApplication(sys.argv)
-window = uic.loadUi("mainform.ui")
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
 
-window.show()
-sys.exit(app.exec())
+    import LoadNiceFile
+    data = LoadNiceFile.loadfile("si.lvm")
+
+    window.ui.matplotlibwidget
+    sys.exit(app.exec_())
